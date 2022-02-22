24a5dc981137087c36f05e1d281116b693b7a393
Batetambe Tanyi <33326060+tanyibate@users.noreply.github.com>
2022-02-22 08:05:06 +0000

    added category selector

---
+++ src/components/SideNavBar/SideNavBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SideNavBar() {
+  return <div>SideNavBar</div>;
+}
--- src/components/category-selector/CategorySelector.jsx
+++ src/components/category-selector/CategorySelector.jsx
@@ -4,16 +4,18 @@ import forwardChevron from "../../assets/icons/forward-chevron.png";
 
 export default function CategorySelector({ image, name }) {
   return (
-    <div className="rounded-lg bg-white-smoke w-full h-40 desktop:h-52 relative cursor-pointer">
+    <div className="rounded-lg bg-white-smoke w-full h-40 desktop:h-48 relative cursor-pointer">
       <img
         src={image}
         alt="category image"
-        className="absolute top-0 right-1/2 transform translate-x-1/2  h-32 desktop:h-40 -translate-y-1/3"
+        className="absolute top-0 right-1/2 transform translate-x-1/2  h-32 desktop:h-44 -translate-y-1/3"
       />
       <div className="absolute bottom-6 right-1/2 transform translate-x-1/2 flex flex-col items-center">
         <h5 className="font-bold text-lg mb-4">{name}</h5>
         <div className="flex items-center">
-          <p className="text-xs mr-2 font-bold hover:text-orange">SHOP</p>
+          <p className="text-xs mr-2 font-bold hover:text-orange opacity-50">
+            SHOP
+          </p>
           <img src={forwardChevron} alt="forward chevron" />
         </div>
       </div>
